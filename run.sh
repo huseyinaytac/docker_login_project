518b380d9c907ebb05224d34f58dcc60cc4bf022
huseyin.aytac <huseyin.aytac@enerji.gov.tr>
2023-03-21 23:13:24 +0300

    Python docker

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 export Browser=firefox
 behave -f allure_behave.formatter:AllureFormatter -o target/allure-results -D browser=$Browser
-allure serve target/allure-results
-#python Utility/generate_allure_report.py
+#allure serve target/allure-results
+python Utility/generate_allure_report.py
 
--- webdriver/web_driver.py
+++ webdriver/web_driver.py
@@ -8,18 +8,18 @@ from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 def get_webdriver(webdriver_type):
-    # if webdriver_type == "firefox":
-    #     driver = webdriver.Firefox(service=GeckoService(GeckoDriverManager().install()))
-    # else:
-    #     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    # return driver
+    if webdriver_type == "firefox":
+        driver = webdriver.Firefox(service=GeckoService(GeckoDriverManager().install()))
+    else:
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    return driver
 
     # selenium_grid##
-        options = webdriver.FirefoxOptions()
-        driver = webdriver.Remote(command_executor="http://localhost:4444/wd/hub",
-                                  desired_capabilities=DesiredCapabilities.FIREFOX, options=options)
-
-        return driver
+    #     options = webdriver.FirefoxOptions()
+    #     driver = webdriver.Remote(command_executor="http://localhost:4444/wd/hub",
+    #                               desired_capabilities=DesiredCapabilities.FIREFOX, options=options)
+    #
+    #     return driver
 
 
 
